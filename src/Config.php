5b99550a112ebff3232c798925baf95e2ac1229c
André Pimpão <a.pimpao@programmator.dev>
2023-08-29 15:25:16 +0100

    feat: added cache config option

--- src/Config.php
+++ src/Config.php
@@ -5,6 +5,7 @@ namespace ProgrammatorDev\SportMonksFootball;
 use ProgrammatorDev\SportMonksFootball\HttpClient\HttpClientBuilder;
 use ProgrammatorDev\YetAnotherPhpValidator\Exception\ValidationException;
 use ProgrammatorDev\YetAnotherPhpValidator\Validator;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class Config
@@ -21,17 +22,17 @@ class Config
         $resolver = new OptionsResolver();
 
         $resolver->setDefaults([
-            'httpClientBuilder' => new HttpClientBuilder()
+            'httpClientBuilder' => new HttpClientBuilder(),
+            'cache' => null
         ]);
 
         $resolver->setRequired('applicationKey');
 
         $resolver->setAllowedTypes('applicationKey', 'string');
         $resolver->setAllowedTypes('httpClientBuilder', HttpClientBuilder::class);
+        $resolver->setAllowedTypes('cache', ['null', CacheItemPoolInterface::class]);
 
-        $resolver->setAllowedValues('applicationKey', function($value) {
-            return !empty($value);
-        });
+        $resolver->setAllowedValues('applicationKey', fn($value) => !empty($value));
 
         return $resolver->resolve($options);
     }
@@ -61,6 +62,18 @@ class Config
     public function setHttpClientBuilder(HttpClientBuilder $httpClientBuilder): self
     {
         $this->options['httpClientBuilder'] = $httpClientBuilder;
+
+        return $this;
+    }
+
+    public function getCache(): ?CacheItemPoolInterface
+    {
+        return $this->options['cache'];
+    }
+
+    public function setCache(?CacheItemPoolInterface $cache): self
+    {
+        $this->options['cache'] = $cache;
 
         return $this;
     }
