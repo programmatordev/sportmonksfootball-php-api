659b485dc23992d708ccdd9f7fdd99e50e648e17
André Pimpão <a.pimpao@programmator.dev>
2023-09-27 17:37:38 +0100

    feat: added collection requests by team id and search query

--- src/Endpoint/SeasonEndpoint.php
+++ src/Endpoint/SeasonEndpoint.php
@@ -12,6 +12,7 @@ use ProgrammatorDev\SportMonksFootball\Entity\Response\SeasonItem;
 use ProgrammatorDev\SportMonksFootball\Exception\ApiErrorException;
 use ProgrammatorDev\SportMonksFootball\Pagination\Pagination;
 use ProgrammatorDev\YetAnotherPhpValidator\Exception\ValidationException;
+use ProgrammatorDev\YetAnotherPhpValidator\Validator;
 
 class SeasonEndpoint extends AbstractEndpoint
 {
@@ -62,5 +63,52 @@ class SeasonEndpoint extends AbstractEndpoint
         );
 
         return new SeasonItem($response);
+    }
+
+    /**
+     * @throws Exception
+     * @throws ApiErrorException
+     */
+    public function getAllByTeamId(int $teamId): SeasonCollection
+    {
+        $response = $this->sendRequest(
+            method: 'GET',
+            path: $this->formatPath('/v3/football/seasons/teams/{teamId}', [
+                'teamId' => $teamId
+            ])
+        );
+
+        return new SeasonCollection($response);
+    }
+
+    /**
+     * @throws Exception
+     * @throws ValidationException
+     * @throws ApiErrorException
+     */
+    public function getAllBySearchQuery(
+        string $query,
+        int $page = 1,
+        int $perPage = Pagination::PER_PAGE,
+        string $order = Pagination::ORDER_ASC
+    ): SeasonCollection
+    {
+        Validator::notBlank()->assert($query, 'query');
+
+        $this->validatePagination($page, $perPage, $order);
+
+        $response = $this->sendRequest(
+            method: 'GET',
+            path: $this->formatPath('/v3/football/seasons/search/{query}', [
+                'query' => $query
+            ]),
+            query: [
+                'page' => $page,
+                'per_page' => $perPage,
+                'order' => $order
+            ]
+        );
+
+        return new SeasonCollection($response);
     }
 }
