ffc129843cafa64b14871cdeb391c55d00fbc916
André Pimpão <a.pimpao@programmator.dev>
2023-10-10 15:29:45 +0100

    feat: added markets endpoint

---
+++ src/Endpoint/MarketEndpoint.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace ProgrammatorDev\SportMonksFootball\Endpoint;
+
+use Http\Client\Exception;
+use ProgrammatorDev\SportMonksFootball\Endpoint\Util\FilterTrait;
+use ProgrammatorDev\SportMonksFootball\Endpoint\Util\SelectTrait;
+use ProgrammatorDev\SportMonksFootball\Endpoint\Util\ValidatorTrait;
+use ProgrammatorDev\SportMonksFootball\Entity\Response\MarketCollection;
+use ProgrammatorDev\SportMonksFootball\Entity\Response\MarketItem;
+use ProgrammatorDev\SportMonksFootball\Exception\ApiErrorException;
+use ProgrammatorDev\SportMonksFootball\Pagination\Pagination;
+use ProgrammatorDev\YetAnotherPhpValidator\Exception\ValidationException;
+
+class MarketEndpoint extends AbstractEndpoint
+{
+    use SelectTrait;
+    use FilterTrait;
+    use ValidatorTrait;
+
+    protected int $cacheTtl = 3600 * 24; // 1 day
+
+    /**
+     * @throws Exception
+     * @throws ValidationException
+     * @throws ApiErrorException
+     */
+    public function getAll(
+        int $page = 1,
+        int $perPage = Pagination::PER_PAGE,
+        string $order = Pagination::ORDER_ASC
+    ): MarketCollection
+    {
+        $this->validatePagination($page, $perPage, $order);
+
+        $response = $this->sendRequest(
+            method: 'GET',
+            path: '/v3/odds/markets',
+            query: [
+                'page' => $page,
+                'per_page' => $perPage,
+                'order' => $order
+            ]
+        );
+
+        return new MarketCollection($response);
+    }
+
+    /**
+     * @throws Exception
+     * @throws ApiErrorException
+     */
+    public function getById(int $id): MarketItem
+    {
+        $response = $this->sendRequest(
+            method: 'GET',
+            path: $this->formatPath('/v3/odds/markets/{id}', [
+                'id' => $id
+            ])
+        );
+
+        return new MarketItem($response);
+    }
+
+    /**
+     * @throws Exception
+     * @throws ValidationException
+     * @throws ApiErrorException
+     */
+    public function getAllBySearchQuery(
+        string $query,
+        int $page = 1,
+        int $perPage = Pagination::PER_PAGE,
+        string $order = Pagination::ORDER_ASC
+    ): MarketCollection
+    {
+        $this->validateSearchQuery($query);
+        $this->validatePagination($page, $perPage, $order);
+
+        $response = $this->sendRequest(
+            method: 'GET',
+            path: $this->formatPath('/v3/odds/markets/search/{query}', [
+                'query' => $query
+            ]),
+            query: [
+                'page' => $page,
+                'per_page' => $perPage,
+                'order' => $order
+            ]
+        );
+
+        return new MarketCollection($response);
+    }
+}
