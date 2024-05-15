a0b1c5eebd8b778371b1a2b24f61c5ca55568e18
André Pimpão <a.pimpao@programmator.dev>
2024-05-15 16:34:14 +0100

    feat: added cache trait

--- src/Resource/Resource.php
+++ src/Resource/Resource.php
@@ -2,15 +2,15 @@
 
 namespace ProgrammatorDev\SportMonksFootball\Resource;
 
-use ProgrammatorDev\SportMonksFootball\Resource\Util\LanguageTrait;
+use ProgrammatorDev\SportMonksFootball\Resource\Util\CacheTrait;
 use ProgrammatorDev\SportMonksFootball\Resource\Util\TimezoneTrait;
 use ProgrammatorDev\SportMonksFootball\Resource\Util\ValidationTrait;
 use ProgrammatorDev\SportMonksFootball\SportMonksFootball;
 
 class Resource
 {
+    use CacheTrait;
     use TimezoneTrait;
-    use LanguageTrait;
     use ValidationTrait;
 
     public function __construct(protected SportMonksFootball $api) {}
