50dd6dd1cfc35b7472a85f8d20bda3cc13d38061
André Pimpão <a.pimpao@programmator.dev>
2025-11-23 17:19:40 +0000

    chore: update libraries

--- src/Util/EntityTrait.php
+++ src/Util/EntityTrait.php
@@ -6,10 +6,10 @@ trait EntityTrait
 {
     private function createEntityCollection(string $entityClass, array $list, ?string $timezone = null): array
     {
-        return \array_map(function(array $data, int|string $key) use ($entityClass, $timezone) {
+        return array_map(function(array $data, int|string $key) use ($entityClass, $timezone) {
             // "_key" index is injected in data to get the key from an associative array response
             $data['_key'] = $key;
             return new $entityClass($data, $timezone);
-        }, $list, \array_keys($list));
+        }, $list, array_keys($list));
     }
 }
